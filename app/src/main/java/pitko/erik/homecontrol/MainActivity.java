edeb89ddcfcccb830903536be3865d55dda1aeec
ErikPitko <pitko.erik@gmail.com>
2018-05-14 21:28:54 +0200

    Basic layout implementation

--- app/src/main/java/pitko/erik/homecontrol/MainActivity.java
+++ app/src/main/java/pitko/erik/homecontrol/MainActivity.java
@@ -3,13 +3,20 @@ package pitko.erik.homecontrol;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private BottomNavigationView navigation;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment homeFragment;
+    private RelayFragment relayFragment;
+    private SettingsFragment settingsFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,26 +25,38 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    setFragment(homeFragment);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_relays:
+                    setFragment(relayFragment);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_settings:
+                    setFragment(settingsFragment);
                     return true;
             }
             return false;
         }
     };
 
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrame, fragment);
+        fragmentTransaction.commit();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        homeFragment = new HomeFragment();
+        relayFragment = new RelayFragment();
+        settingsFragment = new SettingsFragment();
+
+        setFragment(homeFragment);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.mainFrame);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
